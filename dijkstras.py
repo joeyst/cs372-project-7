27609196c5fe14bb475924189016f86b82f63764
joeyst <joeystenbeck@gmail.com>
2022-10-25 20:04:54 -0700

    Add Router class.

--- dijkstras.py
+++ dijkstras.py
@@ -7,13 +7,21 @@ def get_routers_data(path):
 def get_src_dest_pairs(path):
   return read_routers(path)['src-dest']
 
+class Router:
+  def __init__(ip, conns, conn_weights):
+    self.ip = ip
+    self.conns = conns
+    self.conn_weights = conn_weights
+    self.path = []
+    self.path_cost = float('inf')
+
 def display_pairs(pairs):
   print ("   source              dest")
   for source, dest in pairs:
     print("{:18} {:18}".format(source, dest))
 
 def main(argv):
-  routers, pairs = get(argv[1])
+  routers, pairs = get_routers_data(argv[1]), get_src_dest_pairs(argv[1])
   display_pairs(pairs)
 
 if __name__ == "__main__":
