0d136bc306aea139be3ae9b20d77884c8fa6ee43
joeyst <joeystenbeck@gmail.com>
2022-10-26 01:39:46 -0700

    Add function that finds and displays shortest paths.

--- dijkstras.py
+++ dijkstras.py
@@ -1,4 +1,3 @@
-import json
 from netfuncs import *
 from copy import deepcopy
 
@@ -16,19 +15,73 @@ class Network:
   def parse_routers_dict(self, routers_dict):
     parsed_routers = {}
     for curr, data in routers_dict.items():
-      curr_router = Router(curr)
-      curr_router.add_conns(data['connections'])
-      parsed_routers[curr] = curr_router
+      curr_router = make_router(curr, data['netmask'], data['if_count'], data['if_prefix'])
+      curr_router.add_conns(data['connections'], True)
+      parsed_routers[curr] = deepcopy(curr_router)
     return parsed_routers
 
-  def dijk(self, start, finish):
+  def dijk(self, start, finish, display=True, verbose=False):
+    # get start router name, `Router`, and netmask 
+    start_subnet_name = self.find_subnet(start)
+    start_subnet_router = self.routers[start_subnet_name]
+    start_netmask = get_subnet_mask_value(start_subnet_router.netmask)
+
+    # get finish router name, `Router`, and netmask 
+    finish_subnet_name = self.find_subnet(finish)
+    finish_subnet_router = self.routers[finish_subnet_name]
+    finish_netmask = get_subnet_mask_value(finish_subnet_router.netmask)
+
+    # get numeric values of start and finish router masks 
+    start_masked = start_netmask & ipv4_to_value(start)
+    finish_masked = finish_netmask & ipv4_to_value(finish)
+
+    # if have the same router, return immediately 
+    if start_masked == finish_masked:
+      temp_router = Router(start, start_subnet_router.netmask, start_subnet_router.if_count, start_subnet_router.if_prefix, [], [], [], [], 0)
+      if display:
+        print("{:11} {}".format("Sender", start))
+        print("{:11} {}".format("Receiver", finish))
+        temp_router.display(verbose)
+      return temp_router
+
     self.reset()
     assert(len(self.queue) == 0)
 
-    self.queue.append(start)
+    if start not in self.routers.keys():
+      start_router = self.find_subnet(start)
+    else:
+      start_router = start
+
+    if finish not in self.routers.keys():
+      finish_router = self.find_subnet(finish)
+    else:
+      finish_router = finish
+
+    self.routers[start_router].path_cost = 0
+
+    self.queue.append(start_router)
     while (len(self.queue) != 0):
       self.next()
-    return self.routers[finish]
+      while finish_router in self.queue:
+        self.queue.remove(finish_router)
+
+    if start not in self.routers.keys():
+      self.routers[finish_router].path = [start_router] + self.routers[finish_router].path
+
+    if finish not in self.routers.keys():
+      self.routers[finish_router].path.append(finish)
+
+    if display:
+      print("{:11} {}".format("Sender", start))
+      print("{:11} {}".format("Receiver", finish))
+      self.routers[finish_router].display(verbose)
+
+    return self.routers[finish_router]
+
+  def find_subnet(self, ip):
+    for name, router in self.routers.items():
+      if router.equal_subnets(ip):
+        return name
 
   def reset(self):
     for name, router in self.routers.items():
@@ -40,22 +93,23 @@ class Network:
     q = self.queue.pop(0)
 
     update_data = self.routers[q].get_update_data(deepcopy(self.routers))
-    for i in range(len(update_data['conns'])):
-      # keeping for a bit in case code doesn't mutate 
-      # temp_router           = self.routers[update_data['conns'][i]].deepcopy()
-      # temp_router.path      = update_data['paths'][i]
-      # temp_router.path_cost = float(update_data['costs'][i])
-      # self.routers[update_data['conns'][i]] = temp_router
 
+    for i in range(len(update_data['conns'])):
       curr_conn = update_data['conns'][i]
       self.routers[curr_conn].path      = update_data['paths'][i]
       self.routers[curr_conn].path_cost = update_data['costs'][i]
       if not curr_conn in self.queue:
-        queue.append(curr_conn)
+        self.queue.append(curr_conn)
+
+def make_router(ip, netmask, if_count, if_prefix):
+  return deepcopy(Router(ip, netmask, if_count, if_prefix))
 
 class Router:
-  def __init__(self, ip, conns=[], conn_weights=[], interfaces=[], netmasks=[], path_cost=float('inf')):
+  def __init__(self, ip, netmask, if_count, if_prefix, conns=[], conn_weights=[], interfaces=[], netmasks=[], path_cost=float('inf')):
     self.ip = ip
+    self.netmask = netmask
+    self.if_count = if_count
+    self.if_prefix = if_prefix
     self.conns = conns
     self.conn_weights = conn_weights
     self.interfaces = interfaces
@@ -72,7 +126,7 @@ class Router:
     }
 
     for conn, weight, interface in zip(self.conns, self.conn_weights, self.interfaces):
-      if self.faster_path(router_dict[conn].path_cost):
+      if self.faster_path(router_dict[conn].path_cost, weight):
         update_data['conns'].append(conn)
         update_data['costs'].append(self.path_cost + weight)
         update_data['paths'].append(self.path + [conn])
@@ -80,21 +134,42 @@ class Router:
     
     return update_data
 
-  def add_conns(self, conns_data):
-    for conn_data in conns_data.items():
-      self.add_conn(conn_data)
+  def display(self, verbose=False): 
+    print("{:11} {}".format("Rec Router",     self.ip))
+    print("{:11} {}".format("Cost",   self.path_cost))
+    print("{:11} {}".format("Subnet", self.subnet()))
+    print("{:11} {}".format("Path",   self.path))
 
-  def add_conn(self, conn_data):
-    self._assert_lengths()
-    address, data  = conn_data
-    
+    if verbose: 
+      print("")
+      print("{:11} {}".format("Connections", self.conns))
+      print("{:11} {}".format("Weights",     self.conn_weights))
+    print("\n\n\n")
+
+  def subnet(self):
+    return value_to_ipv4(ipv4_to_value(self.ip) & get_subnet_mask_value(self.netmask))
+
+  def equal_subnets(self, other_ip):
+    return ipv4_to_value(self.subnet()) == (ipv4_to_value(other_ip) & get_subnet_mask_value(self.netmask))
+
+  def faster_path(self, other_cost, weight):
+    return(self.path_cost + weight < other_cost)
+
+  def add_conns(self, conns_data, override=False):
+    for conn_data in conns_data.items():
+      self.add_conn(conn_data, override)
+
+  def add_conn(self, conn_data, override=False):
+    self._assert_lengths(override)
+    address, data = conn_data
     self.conns.append(address)
     self.conn_weights.append(float(data['ad']))
     self.interfaces.append(data['interface'])
     self.netmasks.append(data['netmask'])
 
-  def _assert_lengths(self):
-    assert len(self.conns) == len(self.conn_weights) and len(self.conn_weights) == len(self.interfaces) and len(self.interfaces) == len(self.netmasks)
+  def _assert_lengths(self, override=False):
+    if override == False:
+      assert len(self.conns) == len(self.conn_weights) and len(self.conn_weights) == len(self.interfaces) and len(self.interfaces) == len(self.netmasks)
 
 
 def display_pairs(pairs):
@@ -102,12 +177,14 @@ def display_pairs(pairs):
   for source, dest in pairs:
     print("{:18} {:18}".format(source, dest))
 
-def main(argv):
-  routers, pairs = get_routers_data(argv[1]), get_src_dest_pairs(argv[1])
+def find_paths(file_path, display=True, verbose=False):
+  routers, pairs = get_routers_data(file_path), get_src_dest_pairs(file_path)
   network = Network(routers)
-  network.dijk("10.34.52.1", "10.34.166.1")
-  print(network.routers["10.34.166.1"])
-  display_pairs(pairs)
+  for [start, destination] in pairs:
+    network.dijk(start, destination, display=True, verbose=False)
 
+def main(argv):
+  find_paths(argv[1])
+ 
 if __name__ == "__main__":
     sys.exit(main(sys.argv))
